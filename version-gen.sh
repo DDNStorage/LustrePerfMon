43086728030c8e85b6ee94bb3cbcd39b3287f962
Li Xi <lixi@ddn.com>
2014-10-29 19:40:55 +0800

    Add verion to the name of ISO
    The version of ISO is combined by git version and build version.
    Build version can be set by environment variable BUILD_VERSION
    when configuring.
    
    Signed-off-by: Li Xi <lixi@ddn.com>
    Change-Id: I9a8511749d0e0dfa4ca9d2cc96fe750114264493

--- version-gen.sh
+++ version-gen.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-DEFAULT_VERSION="0.1.g$(git rev-parse --short HEAD).ddn1"
-
-if test -z "$VERSION"; then
-	VERSION="$DEFAULT_VERSION"
+MONSYSTEM_VERSION=$(grep MONSYSTEM_VERSION ./version.h  \
+        | awk '{print $3}' | tr \" " " | awk '{print $1}')
+if [ -z "$BUILD_VERSION" ]; then
+        MONSYSTEM_PKGVER=$MONSYSTEM_VERSION.g$(git rev-parse --short HEAD).ddn
+else
+        MONSYSTEM_PKGVER=$MONSYSTEM_VERSION.g$(git rev-parse --short HEAD).ddn.$BUILD_VERSION
 fi
 
-VERSION="`echo \"$VERSION\" | sed -e 's/-/./g'`"
+MONSYSTEM_PKGVER="`echo \"$MONSYSTEM_PKGVER\" | sed -e 's/-/./g'`"
 
-echo -n "$VERSION"
+echo -n "$MONSYSTEM_PKGVER"
---
+++ version.h
@@ -0,0 +1,1 @@
+#define MONSYSTEM_VERSION "0.1"
