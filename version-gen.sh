b204bdc7a6535e5a9497d2ad2cce125e0b935954
Shuichi Ihara <sihara@ddn.com>
2017-07-27 05:21:49 +0900

    Change ISO name with Timestamp
    Chnage RPM name from
    monsystem-ddn.0.1.g<commit>.ddn.<BUILD_NUMBER>.el7.x86_64.iso
    to
    monsystem-ddn.0.1.<BUILDS_ALL_TIME>.el7.x86_64.iso
    
    Change-Id: Ic11b41b58eb04fc2afb016697206fc7b1c6f32e3
    Signed-off-by: Shuichi Ihara <sihara@ddn.com>

--- version-gen.sh
+++ version-gen.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
+TS=$(date +%Y%m%d%H%M%S)
 MONSYSTEM_VERSION=$(grep MONSYSTEM_VERSION ./version.h  \
         | awk '{print $3}' | tr \" " " | awk '{print $1}')
-if [ -z "$BUILD_VERSION" ]; then
-        MONSYSTEM_PKGVER=$MONSYSTEM_VERSION.g$(git rev-parse --short HEAD).ddn
-else
-        MONSYSTEM_PKGVER=$MONSYSTEM_VERSION.g$(git rev-parse --short HEAD).ddn.$BUILD_VERSION
-fi
 
-MONSYSTEM_PKGVER="`echo \"$MONSYSTEM_PKGVER\" | sed -e 's/-/./g'`"
+MONSYSTEM_PKGVER=$MONSYSTEM_VERSION.${TS}
 
 echo -n "$MONSYSTEM_PKGVER"
