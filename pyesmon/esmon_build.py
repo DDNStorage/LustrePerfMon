13a6dbbbf0d3a02105f1e5b7c272b49f6c5c8e1e
Wang Shilong <wshilong@ddn.com>
2020-05-18 09:43:54 +0800

    install qpid-proton-c-devel for newer collectd
    Change-Id: If1698ba0148494a87c3146a00ab3f7e2ac78ae3e
    Signed-off-by: Wang Shilong <wshilong@ddn.com>

--- pyesmon/esmon_build.py
+++ pyesmon/esmon_build.py
@@ -548,7 +548,7 @@ def host_build(workspace, build_host, local_host, collectd_git_path,
                "epel-release perl-Regexp-Common python-pep8 pylint "
                "lua-devel byacc ganglia-devel libmicrohttpd-devel "
                "riemann-c-client-devel xfsprogs-devel uthash-devel "
-               "perl-ExtUtils-Embed -y")
+               "qpid-proton-c-devel perl-ExtUtils-Embed -y")
     retval = build_host.sh_run(command)
     if retval.cr_exit_status:
         logging.error("failed to run command [%s] on host [%s], "
